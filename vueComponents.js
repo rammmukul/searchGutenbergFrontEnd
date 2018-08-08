dbd66f4807ea1113c4bf232c96d0b24cc5bd9040
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-08 19:14:16 +0530

    bind advancedSearch's data

--- vueComponents.js
+++ vueComponents.js
@@ -11,12 +11,17 @@ const advancedSearchComponentOptions = {
                 <legend>Advanced Search</legend>
                 <button @click="$emit('switch-form')">Back to Basic Search</button>
                 <label for="title">Title: </label>
-                <input type="text" id="title">
+                <input v-model="title" type="text" id="title">
                 <label for="author">Author: </label>
-                <input type="text" id="author">
+                <input v-model="author" type="text" id="author">
                 <label for="subject">Subject: </label>
-                <input type="text" id="subject">
+                <input v-model="subject" type="text" id="subject">
                 <button>Search</button>
               </form>
-            </div>`
+            </div>`,
+  data: () => ({
+    title: '',
+    author: '',
+    subject: ''
+  })
 }
