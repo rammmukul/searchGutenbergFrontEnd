3f1c97e176d6afb194dd7c84542548dd20b2c8a1
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-08 17:59:33 +0530

    move main template form html to js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const appOptions = {
+  template: `
+    <div>
+      <h1>Search Gutenberg</h1>
+      <div v-if="basicSearchOn">
+        <basic-search
+          v-on:switch-form="switchForm"
+          v-on:input="changeText" />
+      </div>
+      <div v-else>
+        <advanced-search
+          v-on:switch-form="switchForm" />
+      </div>
+    </div>`,
   el: '#app',
   data: {
     search: '',
