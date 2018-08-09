b90bd14bda0fd907a5c071bf6b303c1a110b7bfa
Mukul Mahajan Singraur <rammmukul@outlook.com>
2018-08-09 13:25:21 +0530

    add book component

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ const appOptions = {
   data: {
     basicSearch: '',
     basicSearchOn: true,
-    queryString: ''
+    queryString: '',
+    bookObj: {'title': 'hmara'}
   },
   methods: {
     switchForm (message) {
@@ -42,4 +43,5 @@ const appOptions = {
 
 Vue.component('basic-search', searchComponentOptions)
 Vue.component('advanced-search', advancedSearchComponentOptions)
+Vue.component('book', bookComponentOptions)
 const app = new Vue(appOptions)
